cb6a1bf97d0e1847cdf513cbd6f4bf3255ffa535
Jake Abed <jakeabed@proton.me>
2024-12-19 14:17:18 -0800

    Fix borked styling from previous commit

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ import (
 	"database/sql"
 	"embed"
 	"io"
-	"time"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -90,8 +90,8 @@ func main() {
 
 	router.Mount("/v1", v1Router)
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
 		ReadHeaderTimeout: time.Second * 5,
 	}
 
